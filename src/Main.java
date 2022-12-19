b87242d6f51b05afb553adc93f1512e307edadcd
Mila <milaprivalova1@gmail.com>
2022-12-19 12:51:40 +0300

    tree

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Person irina = new Person("Ирина");
-        Person vasya = new Person("Вася");
+        Person vasya = new Person("Валя");
         Person masha = new Person("Маша");
         Person jane = new Person("Женя");
         Person ivan = new Person("Ваня");
